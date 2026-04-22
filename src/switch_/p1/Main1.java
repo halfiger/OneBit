cc2a9e465ac877967c311d556e0a14ca9382b902
FUJI <jazzmaster204@gmail.com>
2026-04-22 16:48:54 +0300

    [Switch] 1.17 /practice/ new switch java 14+

--- src/switch_/p1/Main1.java
+++ src/switch_/p1/Main1.java
@@ -266,4 +266,16 @@ public class Main1 {
             default -> "Fault";
         };
     }
+
+    public String getSeason (int month) {
+        return switch (month) {
+            case 12,1,2 -> "Winter";
+            case 3,4,5 -> "Spring";
+            case 6,7,8 -> "Summer";
+            case 9,10,11 -> "Autumn";
+            default -> "none";
+        };
+    }
+
+
 }
