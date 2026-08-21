fd1ba9f3aebce930099ee77288a6f8c7787c824d
FUJI <jazzmaster204@gmail.com>
2026-08-21 23:57:13 +0300

    [Stream api] /practice/  | unit2

---
+++ src/stream_api/unit2/stream15_grouping/Main2.java
@@ -0,0 +1,28 @@
+package stream_api.unit2.stream15_grouping;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main2 {
+
+    public Map<String, Boolean> task1 (String [] words) {
+        return Arrays.stream(words)
+                .collect(Collectors.toMap(Function.identity(), Main2::check));
+    }
+
+    public static boolean check(String s) {
+        return s.chars().anyMatch(c->"aeiuo".indexOf(c)>=0);
+    }
+
+    public Map <Character, Long> task2 (String s) {
+        return s.toLowerCase()
+                .chars()
+                .mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+
+
+}
